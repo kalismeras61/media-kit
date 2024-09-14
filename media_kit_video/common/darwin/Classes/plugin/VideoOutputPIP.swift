ee43b58245e1bd43ebc6265a5c5153650c090109
kalismeras61 <yasinilhan61@gmail.com>
2024-09-14 08:49:07 +0300

    added new way 8

--- media_kit_video/common/darwin/Classes/plugin/VideoOutputPIP.swift
+++ media_kit_video/common/darwin/Classes/plugin/VideoOutputPIP.swift
@@ -50,9 +50,7 @@ public class VideoOutputPIP: VideoOutput, AVPictureInPictureSampleBufferPlayback
         }
 
         if pipController!.canStartPictureInPictureAutomaticallyFromInline || pipController!.isPictureInPictureActive {
-             worker.enqueue {
-                 self.switchToSoftwareRendering()
-              }
+            self.switchToSoftwareRendering()
             return
         }
 
